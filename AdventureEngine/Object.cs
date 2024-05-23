cf590467be05ba06b1e4d7aed10ba7960c29116e
Cary-Kraus <kraus-c@list.ru>
2024-05-24 01:31:52 +0300

    Удалены ненужные using

--- AdventureEngine/Object.cs
+++ AdventureEngine/Object.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AdventureEngine;
+﻿using AdventureEngine;
 
 namespace DREAMS
 {
